9dfcc976041a14bf2d3791d04e36791e2c47d009
algarmeclarkjustin <algarmeclarkjustin@gmail.com>
2026-08-11 19:50:10 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const txtNum = document.getElementById("txtNum");
+
+let array = [];
+
+function insertNumbers() {
+    let value = txtNum.value.trim();
+    if (value === "") {
+        return
+    }
+}
